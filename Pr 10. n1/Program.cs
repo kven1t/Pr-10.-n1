902076e90373085e6be1b7f225ab66e9916d05c9
hp <hp@HP>
2022-11-18 12:44:38 +0300

    Добавьте файлы проекта.

---
+++ Pr 10. n1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Pr_10._n1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double z, x, n;
+            z = 0;
+            Console.Write("x= ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("(n>0), n= ");
+            n = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n + 1; i++) z += Math.Pow((-1),i-1) * (Math.Pow(x, (2*i + 1))/ ((2*i + 1)*(2*(i-1) + 1)));
+            Console.WriteLine("z = {0}", z);
+        }
+    }
+}
